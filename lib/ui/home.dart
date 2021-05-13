79ab77113f377d0df3bca2c005b2df7ce4605874
monakhov <monakhov@krista.ru>
2021-05-13 13:27:42 +0300

    Исправление отображения темы

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'package:api_event/api_event.dart';
 import 'package:crypt_signature/bloc/native.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'certificates.dart';
 
@@ -31,51 +32,57 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(250, 250, 250, 1),
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40),
-        child: AppBar(
+    return MaterialApp(
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: Scaffold(
           backgroundColor: Color.fromRGBO(250, 250, 250, 1),
-          centerTitle: true,
-          elevation: 0,
-          title: Text(
-            widget.title,
-            style: TextStyle(color: Colors.black),
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(40),
+            child: AppBar(
+              backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+              centerTitle: true,
+              elevation: 0,
+              title: Text(
+                widget.title,
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () => Navigator.of(context).pop(),
+                child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "Назад",
+                      maxLines: 1,
+                      style: TextStyle(color: Colors.redAccent),
+                    )),
+              ),
+            ),
           ),
-          leading: GestureDetector(
-            behavior: HitTestBehavior.translucent,
-            onTap: () => Navigator.of(context).pop(),
-            child: Container(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "Назад",
-                  maxLines: 1,
-                  style: TextStyle(color: Colors.redAccent),
-                )),
-          ),
+          body: StreamBuilder<bool>(
+              stream: csp.stream,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(
+                    child: Container(
+                      width: 20.0,
+                      height: 20.0,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 1,
+                        backgroundColor: Colors.transparent,
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                      ),
+                    ),
+                  );
+
+                return snapshot.data
+                    ? Certificates(hint: widget.hint)
+                    : Center(
+                        child: Text("Не удалось инициализировать провайдер"));
+              }),
         ),
       ),
-      body: StreamBuilder<bool>(
-          stream: csp.stream,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData)
-              return Center(
-                child: Container(
-                  width: 20.0,
-                  height: 20.0,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 1,
-                    backgroundColor: Colors.transparent,
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                  ),
-                ),
-              );
-
-            return snapshot.data
-                ? Certificates(hint: widget.hint)
-                : Center(child: Text("Не удалось инициализировать провайдер"));
-          }),
     );
   }
 }
